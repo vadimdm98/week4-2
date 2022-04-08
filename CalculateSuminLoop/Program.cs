7dc4b03668b87a9af69d17be8126b873b03719f9
vadimdm98 <101522820+vadimdm98@users.noreply.github.com>
2022-04-08 10:15:18 +0300

    Add files via upload

---
+++ CalculateSuminLoop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CalculateSuminLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd= new Random();
+            int sum = 0;
+            for (int i = 1; i < 11; i++) 
+            {
+                int myRandom = rnd.Next(1, 11);
+                sum = sum + myRandom;
+                Console.WriteLine($"{i} Random nr: {myRandom}");
+            }
+            Console.WriteLine($"Summa = {sum}",sum);
+        }
+    }
+}
